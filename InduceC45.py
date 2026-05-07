f457f42de3e8eed500825829866844d7467a5353
julienfiechter <jfiechte@calpoly.edu>
2026-05-07 09:43:06 -0700

    updated load input files

--- InduceC45.py
+++ InduceC45.py
@@ -3,18 +3,45 @@ import json
 import pandas as pd
 from c45 import C45
 
-def load_data(filename):
-    meta = pd.read_csv(filename, nrows=3, header=None)
+def load_input_file(filename):
+    preview = pd.read_csv(filename, nrows=3, header=None)
+    row1 = preview.iloc[1].dropna().astype(str).str.strip()
 
-    class_col = meta.iloc[2].dropna().values[0]
+    lab2_format = True
+    for value in row1:
+        try:
+            int(value)
+        except ValueError:
+            lab2_format = False
+            break
 
-    df = pd.read_csv(filename, skiprows=[1, 2])
-    df.columns = df.columns.str.strip()
+    if lab2_format:
+        meta = preview
+        class_col = meta.iloc[2].dropna().values[0].strip()
+
+        df = pd.read_csv(filename, skiprows=[1, 2])
+        df.columns = df.columns.str.strip()
+        type_row = meta.iloc[1].tolist()
+        drop_cols = []
+
+        for col, col_type in zip(df.columns, type_row):
+            try:
+                if int(col_type) == -1:
+                    drop_cols.append(col)
+            except ValueError:
+                pass
+
+        df = df.drop(columns=drop_cols)
+
+    else:
+        df = pd.read_csv(filename)
+        df.columns = df.columns.str.strip()
+        class_col = df.columns[-1]
 
     X = df.drop(columns=[class_col])
     y = df[class_col]
 
-    return X, y, class_col
+    return X, y
 
 def main():
     if len(sys.argv) < 2:
--- crossValSKL.py
+++ crossValSKL.py
@@ -10,10 +10,40 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 
 def load_input_file(filename):
-    meta = pd.read_csv(filename, nrows=3, header=None)
-    class_col = meta.iloc[2].dropna().values[0].strip()
-    df = pd.read_csv(filename, skiprows=[1, 2])
-    df.columns = df.columns.str.strip()
+    preview = pd.read_csv(filename, nrows=3, header=None)
+    row1 = preview.iloc[1].dropna().astype(str).str.strip()
+
+    lab2_format = True
+    for value in row1:
+        try:
+            int(value)
+        except ValueError:
+            lab2_format = False
+            break
+
+    if lab2_format:
+        meta = preview
+        class_col = meta.iloc[2].dropna().values[0].strip()
+
+        df = pd.read_csv(filename, skiprows=[1, 2])
+        df.columns = df.columns.str.strip()
+        type_row = meta.iloc[1].tolist()
+        drop_cols = []
+
+        for col, col_type in zip(df.columns, type_row):
+            try:
+                if int(col_type) == -1:
+                    drop_cols.append(col)
+            except ValueError:
+                pass
+
+        df = df.drop(columns=drop_cols)
+
+    else:
+        df = pd.read_csv(filename)
+        df.columns = df.columns.str.strip()
+        class_col = df.columns[-1]
+
     X = df.drop(columns=[class_col])
     y = df[class_col]
 
